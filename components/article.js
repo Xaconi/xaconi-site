faaece74f5e962b24d1cf9812a323d46e8952389
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-02-15 22:28:19 +0100

    Back to img tag on post page.

--- components/article.js
+++ components/article.js
@@ -10,18 +10,19 @@ import postStyles from '../styles/Post.module.css';
 export default function Article({title, image, content}) {
     return(
         <section className={postStyles.post}>
-            <Image 
-                height="320" 
-                layout="responsive" 
+            <img 
+                alt={title}
+                className={postStyles.img}
+                height="260" 
                 src={ image } 
-                width="720" 
+                width="640" 
             />
             <h1>{ title }</h1>
             <Markdown>{ content }</Markdown>
             <div className={postStyles.share}>
                 Comparte!
                 <a href="https://twitter.com/xaconi" target="_blank">
-                    <img src="/twitter_logo.svg" width="24" height="24" />
+                    <img src="/twitter_logo.svg" width="18" height="18" />
                 </a>
             </div>
         </section>
