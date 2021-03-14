d849221562beff32cca289d9cfe0ba36c6b04df0
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-03-14 16:02:37 +0100

    Added dynamic image thumbnails to posts for social media shares.

--- components/custom-head.js
+++ components/custom-head.js
@@ -1,12 +1,7 @@
 // NextJS Core
 import Head from 'next/head';
 
-// Hooks
-import useGetDomain from '../hooks/useGetDomain';
-
-export default function CustomHead({ title, description, image, link, type}) {
-    
-    const domain = useGetDomain();
+export default function CustomHead({ title, description, image, link, type, domain}) {
 
     return(
         <Head>
@@ -16,10 +11,10 @@ export default function CustomHead({ title, description, image, link, type}) {
             <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"></link>
             
             {/* FACEBOOK */}
-            <meta property="og:url" content={`${link}`} />
+            <meta property="og:url" content={ link } />
             <meta property="og:title" content={ title } />
             <meta property="og:description" content={ description } />
-            <meta property="og:image" content={ `${image}` } />
+            <meta property="og:image" content={ image } />
             <meta property="og:type" content={ type } />
             <meta property="og:locale" content="es_ES" />
             <meta property="og:site_name" content="xaconi.dev" />
@@ -28,7 +23,7 @@ export default function CustomHead({ title, description, image, link, type}) {
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:creator" content="@xaconi" />
             <meta name="twitter:domain" content={ domain } />
-            <meta name="twitter:image" content={ `${image}` } />
+            <meta name="twitter:image" content={ image } />
             <meta name="twitter:site" content="@xaconi" />
 
             {/* MANIFEST */}
--- pages/posts.js
+++ pages/posts.js
@@ -30,6 +30,7 @@ export default function Posts({ posts }) {
                 image="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
                 link={ `${domain}/posts` }
                 type="web"
+				domain= { useGetDomain('') }
             >
             </CustomHead>
 
