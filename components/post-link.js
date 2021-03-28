f4db0d3c67f7f7be6ce4356c615bb63d4fcc423b
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-03-28 16:39:40 +0200

    Added security and SEO tags to PostLink component.

--- components/post-link.js
+++ components/post-link.js
@@ -8,9 +8,11 @@ export default function PostLink(props) {
     const isExternalLink = props.href.indexOf(getBaseURL('')) == -1;
     return(
         <a 
-            target={isExternalLink ? '_blank' : ''}
             className={isExternalLink ? stylesLink.externalLink : ''}
             href={props.href}
+            rel={isExternalLink ? 'noreferrer noopener nofollow' : ''}
+            target={isExternalLink ? '_blank' : ''}
+            title={props.children[0]}
         >
             {props.children[0]}
         </a>
