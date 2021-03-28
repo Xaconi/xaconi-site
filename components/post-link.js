bada1a3ca8ac232ed58a8a8a670009b7161f8d84
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-03-28 14:11:27 +0200

    Added styles to the PostLink component.

--- components/post-link.js
+++ components/post-link.js
@@ -1,11 +1,15 @@
 // Services
 import getBaseURL from '../services/urlService';
 
+// Styles
+import stylesLink from '../styles/Post.module.css';
+
 export default function PostLink(props) {
     const isExternalLink = props.href.indexOf(getBaseURL('')) == -1;
     return(
         <a 
             target={isExternalLink ? '_blank' : ''}
+            className={isExternalLink ? stylesLink.externalLink : ''}
             href={props.href}
         >
             {props.children[0]}
