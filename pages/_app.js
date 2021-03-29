d2bd3bbc69be1059a7c5c3dd1972b4b3d4294f47
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-03-29 22:43:11 +0200

    Blocked GA on development environment.

--- pages/_app.js
+++ pages/_app.js
@@ -8,19 +8,21 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      /* invoke analytics function only for production */
-      ga('send', 'pageview', url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-  return <Component {...pageProps} />
+	if(process.env.NODE_ENV != 'development') {
+		useEffect(() => {
+			const handleRouteChange = (url) => {
+				/* invoke analytics function only for production */
+				ga('send', 'pageview', url);
+			};
+			router.events.on("routeChangeComplete", handleRouteChange);
+			return () => {
+				router.events.off("routeChangeComplete", handleRouteChange);
+			};
+		}, [router.events]);
+	}
+	return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -8,20 +8,21 @@ class MyDocument extends Document {
     }
 
     render() {
+        console.log();
         return (
             <Html lang="es">
                 <Head />
                 <body>
                     <Main />
                     <NextScript />
-                    <script defer
+                    {process.env.NODE_ENV != 'development' && <script defer
                         dangerouslySetInnerHTML={{
                             __html: `
                                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                                ga('create', 'UA-135636972-1', 'jnjsite.com', {
+                                ga('create', 'UA-135636972-1', 'xaconi.dev', {
                                   'storage': 'none',
                                   'anonymizeIp': true,
                                   'clientId': window.localStorage.getItem('googleAnalyticsClientId')
@@ -32,7 +33,7 @@ class MyDocument extends Document {
                                 ga('send', 'pageview');
                             `,
                         }}
-                    />
+                    />}
                 </body>
             </Html>
         )
