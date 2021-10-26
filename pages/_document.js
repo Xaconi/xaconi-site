91cf3ba1d1f578aaa26750df7f010f484c811867
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2021-10-26 23:03:06 +0200

    Fix GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,6 @@
 // NextJS
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-// Components
-import GoogleAnalytics from '../components/ga'
-
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx);
@@ -17,7 +14,25 @@ class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
-                    <GoogleAnalytics />
+                    {process.env.NODE_ENV != 'development' && <script defer
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                                ga('create', 'UA-135636972-1', 'xaconi.dev', {
+                                  'storage': 'none',
+                                  'anonymizeIp': true,
+                                  'clientId': window.localStorage.getItem('googleAnalyticsClientId')
+                                });
+                                ga(function(tracker) {
+                                  window.localStorage.setItem('googleAnalyticsClientId', tracker.get('clientId'));
+                                });
+                                ga('send', 'pageview');
+                            `,
+                        }}
+                    />}
                 </body>
             </Html>
         )
